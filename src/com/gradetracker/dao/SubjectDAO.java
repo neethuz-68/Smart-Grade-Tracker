06cd04350fd5d2895cc1851bd65d4e47c9962862
Niya-sabu <niya.ns1112@gmail.com>
2025-10-18 18:21:53 +0530

    Update SubjectDAO.java

--- src/com/gradetracker/dao/SubjectDAO.java
+++ src/com/gradetracker/dao/SubjectDAO.java
@@ -1,13 +1,12 @@
-package com.gradetracker.dao;
+package dao;
 
-import com.gradetracker.model.Subject;
 import java.util.List;
+import model.Subject;
 
 public interface SubjectDAO {
-    /**
-     * Retrieves a list of all subjects for a specific semester.
-     * @param semesterId The ID of the semester.
-     * @return A list of Subject objects, populated with their grade info.
-     */
-    List<Subject> getSubjectsForSemester(int semesterId);
+    // Fetch a subject by its name
+    Subject getSubjectByName(String subjectName);
+
+    // Retrieve all subjects
+    List<Subject> getAllSubjects();
 }
